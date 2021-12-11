fed2d10f8cbe2d19a2e32031c0489d0aa8d1bb51
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2021-12-11 19:55:44 +0100

    updated colors

--- colors.py
+++ colors.py
@@ -1,17 +1,44 @@
-BACKGROUND = (255, 255, 230)
+BACKGROUND_DARK = (38, 41, 46)            # dark gray
+BACKGROUND_LIGHT = (255, 245, 232)        # cream white
+BACKGROUND_TABLE = (154, 150, 171)        # dark blue-gray
 
-#TODO: define colors for buttons and input boxes
+PLAYER_HUMAN = (255, 181, 234)            # light pink
+PLAYER_COMPUTER = (179, 207, 255)         # light blue
 
-PLAYER1_STARTTILE = (100, 100, 100)
-PLAYER1_FIGURE = (30, 30, 30)
+PLAYER1_STARTTILE = (100, 100, 100)       # gray 
+PLAYER1_FIGURE = (30, 30, 30)             # dark gray
 
-PLAYER2_STARTTILE = (240, 240, 240)
-PLAYER1_FIGURE = (250, 250, 250)
+PLAYER2_STARTTILE = (240, 240, 240)       # grayed white
+PLAYER1_FIGURE = (250, 250, 250)          # white
 
-WALL_BLUE = (160, 190, 250)
-WALL_GREEN = (160, 250, 190)
 
-MESSAGE_ERROR = (173, 69, 87)
-MESSAGE_SUCCESS = (76, 135, 50)
-MESSAGE_BASIC = (50, 79, 125)
-MESSAGE_NOTIFICATION = (191, 101, 36)
+WALL_BLUE = (160, 190, 250)               # sharp blue
+WALL_GREEN = (160, 250, 190)              # sharp green
+
+TEXT_LIGHT = (235, 231, 221)              # yellowish white
+TEXT_DARK = (26, 27, 28)                  # darker gray
+
+MESSAGE_BASIC = (50, 79, 125)             # blue
+MESSAGE_SUCCESS = (76, 135, 50)           # green 
+MESSAGE_NOTIFICATION = (191, 101, 36)     # orange
+MESSAGE_ERROR = (173, 69, 87)             # red
+
+BUTTON_BASIC = (237, 248, 255)            # light blue-white
+BUTTON_SUCCESS = (203, 255, 171)          # pastel green
+BUTTON_WARNING = (245, 198, 122)          # pastel orange
+BUTTON_DANGER = (250, 160, 160)           # pastel red
+
+BUTTON_HOVER_BASIC = (195, 219, 235)      # darker light blue-white
+BUTTON_HOVER_SUCCESS = (155, 219, 116)    # darker pastel green
+BUTTON_HOVER_WARNING = (214, 159, 71)     # darker pastel orange
+BUTTON_HOVER_DANGER = (222, 109, 109)     # darker pastel red
+
+BUTTON_CLICKED_BASIC = (149, 173, 189)    # stronger light blue-white
+BUTTON_CLICKED_SUCCESS = (120, 168, 91)   # stronger pastel green
+BUTTON_CLICKED_WARNING = (168, 129, 66)   # stronger pastel orange
+BUTTON_CLICKED_DANGER = (181, 94, 94)     # stronger pastel red
+
+BUTTON_DISABLED_BASIC = (121, 126, 130)   # grayed light blue-white
+BUTTON_DISABLED_SUCCESS = (110, 135, 95)  # grayed pastel green
+BUTTON_DISABLED_WARNING = (130, 107, 78)  # grayed pastel orange
+BUTTON_DISABLED_DANGER = (143, 94, 94)    # grayed pastel red

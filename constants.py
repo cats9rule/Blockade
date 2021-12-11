121d71d2470ee474ce09481d290e28fbc34a32cd
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2021-12-11 22:53:00 +0100

    update

--- constants.py
+++ constants.py
@@ -4,12 +4,4 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
 TABLE_CELL = 20
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 FPS = 60
-=======
-FPS = 60
->>>>>>> eb9ffbd (update)
-=======
-FPS = 60
->>>>>>> eb9ffbd60ccd76ef975efe84148c72f71f0b2a0d

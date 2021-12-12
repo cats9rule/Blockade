c210a749d198b220dc4079402161a57d3ffd8de1
welcomepeople <djoksy99@gmail.com>
2021-12-12 19:17:10 +0100

    commit1

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 import colors
 import constants
+import gameInit
 
 WIN = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
 pygame.display.set_caption("Blockade")
+GAME_INIT = gameInit.InitGame()
+
 def draw_window():
     WIN.fill(colors.BACKGROUND_DARK)
+    GAME_INIT.draw(WIN)
     pygame.display.update()
 
+# def draw_label(text, size, pos):
+#     lab = label.Label(text, size, colors.TEXT_LIGHT)
+#     WIN.blit(lab.text, pos)
 
 def main():
     clock = pygame.time.Clock() 

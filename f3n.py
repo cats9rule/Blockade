517dd2a5235390b8f5bd86ca55bbf10cd11864da
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2022-01-01 16:54:26 +0100

    added computer_move, fixed main

---
+++ f3n.py
@@ -0,0 +1,4 @@
+def computer_move(state: dict, starting_pos: dict, board_dim: tuple):
+    #TODO: play minmax, get new state, return new state
+    new_state = state
+    return new_state
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import faza1
 import faza2
+import f3n
 import pyfiglet
 import copy
 
@@ -25,11 +26,16 @@ def main():
         move = faza2.input_move(state, board_dim, starting_pos)
         state = faza1.generate_next_state(state, move)
         faza1.show_game(state, board_dim, starting_pos)
+        state = f3n.computer_move(state, starting_pos, board_dim)
+        faza1.show_game(state, board_dim, starting_pos)
 
     winner = faza2.get_winner(state)
     result = pyfiglet.figlet_format(f"Winner! {winner} won this game!")
     faza1.cls()
     print(result)
+    
+    print(faza2.is_path_available(state, starting_pos, board_dim)) 
+    
     return
 
 main()
